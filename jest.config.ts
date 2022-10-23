1a324e74c9542f56cddf699815dda01557eeb2fe
Bob Lou <louxinbo@gmail.com>
2022-10-23 19:59:23 +1100

    chores

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { defaults as tsjPreset } from 'ts-jest/presets';
 import type { JestConfigWithTsJest } from 'ts-jest';
 
@@ -9,11 +10,17 @@ const jestConfig: JestConfigWithTsJest = {
       'ts-jest',
       {
         tsconfig: 'tsconfig.spec.json',
+        diagnostics: {
+          exclude: ['**'],
+        },
       },
     ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
 };
 
 export default jestConfig;
